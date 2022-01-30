006f8133f2769a974bcd38bc746a8c120b15d827
JulesFiliot <jules.filiot@cpe.fr>
2022-01-30 16:22:00 -0500

    [ADD] db config + eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // personal rules :
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error']
   },
 };
