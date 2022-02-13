c01e8af03f12182254eb315c07e49f73f5fc0c1c
JulesFiliot <jules.filiot@cpe.fr>
2022-02-12 22:53:40 -0500

    [CHANGE] swagger doc tags

--- src/roadmap/roadmap.controller.ts
+++ src/roadmap/roadmap.controller.ts
@@ -6,58 +6,67 @@ import { UpdateRoadMapDto } from './dto/update-roadmap.dto';
 import { UpdateTimeStampDto } from './dto/update-timeStamp.dto';
 import { RoadmapService } from './roadmap.service';
 
-@ApiTags('roadmaps')
 @Controller('roadmaps')
 export class RoadmapController {
   constructor(private readonly roadmapService: RoadmapService) {}
 
   // TIME STAMPS
+  @ApiTags('time stamps')
   @Post('/timestamps')
   createTimeStamp(@Body() createTimeStampDto: CreateTimeStampDto) {
     return this.roadmapService.createTimeStamp(createTimeStampDto);
   }
 
+  @ApiTags('time stamps')
   @Get('/timestamps')
   findAllTimeStamps () {
     return this.roadmapService.findAllTimeStamp();
   }
 
+  @ApiTags('time stamps')
   @Get('/timestamps/:id')
   findOneTimeStamp(@Param('id') id: string) {
     return this.roadmapService.findOneTimeStamp(id);
   }
 
+  @ApiTags('time stamps')
   @Patch('/timestamps/:id')
   updateTimeStamp(@Param('id') id: string, @Body() updateTimeStampDto: UpdateTimeStampDto) {
     return this.roadmapService.updateTimeStamp(id, updateTimeStampDto);
   }
 
+  @ApiTags('time stamps')
   @Delete('/timestamps/:id')
   removeTimeStamp(@Param('id') id: string) {
     return this.roadmapService.removeTimeStamp(id);
   }
 
   // ROADMAPS
+  @ApiTags('roadmaps')
   @Post()
   createRoadmap(@Body() createRoadmapDto: CreateRoadmapDto) {
     return this.roadmapService.createRoadmap(createRoadmapDto);
   }
 
+  @ApiTags('roadmaps')
   @Get()
   findAllRoadmap() {
     return this.roadmapService.findAllRoadmap();
   }
 
+  @ApiTags('roadmaps')
   @Patch(':id')
   updateRoadmap(@Param('id') id: string, @Body() updateRoadMapDto: UpdateRoadMapDto) {
     return this.roadmapService.updateRoadmap(id, updateRoadMapDto);
   }
 
+  @ApiTags('roadmaps')
   @Get(':id')
   findOneRoadmap(@Param('id') id: string) {
     return this.roadmapService.findOneRoadmap(id);
   }
 
+  @ApiTags('roadmaps')
   @Delete(':id')
   removeRoadmap(@Param('id') id: string) {
     return this.roadmapService.removeRoadmap(id);
--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -5,53 +5,61 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
 import { SkillService } from './skill.service';
 
-@ApiTags('skills')
 @Controller('skills')
 export class SkillController {
   constructor(private readonly skillService: SkillService) {}
 
   // CATEGORIES
+  @ApiTags('categories')
   @Get('/categories')
   findAllCategories() {
     return this.skillService.findAllCategories();
   }
 
+  @ApiTags('categories')
   @Patch('/categories/:id')
   updateCategory(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.skillService.updateCategory(id, updateCategoryDto);
   }
 
+  @ApiTags('categories')
   @Get('/categories/:id')
   findOneCategory(@Param('id') id: string) {
     return this.skillService.findOneCategory(id);
   }
 
+  @ApiTags('categories')
   @Delete('/categories/:id')
   removeCategory(@Param('id') id: string) {
     return this.skillService.removeCategory(id);
   }
 
   // SKILLS
+  @ApiTags('skills')
   @Post()
   createSkill(@Body() createSkillDto: CreateSkillDto) {
     return this.skillService.createSkill(createSkillDto);
   }
 
+  @ApiTags('skills')
   @Get()
   findAllSkills() {
     return this.skillService.findAllSkills();
   }
 
+  @ApiTags('skills')
   @Patch(':id')
   updateSkill(@Param('id') id: string, @Body() updateSkillDto: UpdateSkillDto) {
     return this.skillService.updateSkill(id, updateSkillDto);
   }
 
+  @ApiTags('skills')
   @Get(':id')
   findOneSkill(@Param('id') id: string) {
     return this.skillService.findOneSkill(id);
   }
 
+  @ApiTags('skills')
   @Delete(':id')
   removeSkill(@Param('id') id: string) {
     return this.skillService.removeSkill(id);
