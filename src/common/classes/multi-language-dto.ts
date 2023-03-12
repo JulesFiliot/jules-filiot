da42b6966e6a73b09ac006222dd29eba9da0818d
JulesFILIOT <dev@julesfiliot.com>
2023-03-12 18:54:01 +0900

    [FIX] Some DTO schemas not showing on swagger

--- src/common/classes/multi-language-dto.ts
+++ src/common/classes/multi-language-dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class MultiLanguageDTO {
+  @ApiProperty({ description: 'French translation of property' })
   @IsString()
   @IsNotEmpty()
     fr: string;
-
+  
+  @ApiProperty({ description: 'English translation of property' })
   @IsString()
   @IsNotEmpty()
     en: string;
