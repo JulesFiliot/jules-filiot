998992ba4f4042d87dcded59ed0cd8519ab7dffe
JulesFILIOT <dev@julesfiliot.com>
2023-05-22 18:58:15 +0900

    [CHANGE] cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'julesfiliot.com' });
+  app.enableCors({ origin: 'https://julesfiliot.com' });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
